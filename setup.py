564ed86b34d504da638562f233efc59e441f1111
Morten Knutsen <morten.knutsen@uninett.no>
2014-01-21 14:33:11 +0100

    Bypass...

--- setup.py
+++ setup.py
@@ -2,20 +2,13 @@
 
 from distutils.core import setup
 
-
-def read(filename):
-    return open(filename, 'rb').read().decode('utf-8')
-
 setup(name="etcd-py",
       version="0.0.6",
       description="Client for Etcd",
-      #long_description=read("README.rst"),
       author="Kris Foster",
       author_email="kris.foster@gmail.com",
       maintainer="Kris Foster",
       maintainer_email="kris.foster@gmail.com",
-      url="https://github.com/transitorykris/etcd-py",
-      download_url="https://github.com/transitorykris/etcd-py/archive/master.zip",
       classifiers=("Development Status :: 3 - Alpha",
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: MIT License",
@@ -23,6 +16,5 @@ setup(name="etcd-py",
                    "Programming Language :: Python",
                    "Programming Language :: Python :: 2.6",
                    "Programming Language :: Python :: 2.7"),
-      #license=read("LICENSE"),
       packages=['etcd'],
       )
